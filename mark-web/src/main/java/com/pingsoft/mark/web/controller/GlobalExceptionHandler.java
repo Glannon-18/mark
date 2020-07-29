3ef7f355018b388193b43485ae99c01284431bf7
myName <rced87@aliyun.com>
2020-07-29 15:06:35 +0800

    更改全局异常提醒

--- mark-web/src/main/java/com/pingsoft/mark/web/controller/GlobalExceptionHandler.java
+++ mark-web/src/main/java/com/pingsoft/mark/web/controller/GlobalExceptionHandler.java
@@ -1,19 +1,16 @@
 package com.pingsoft.mark.web.controller;
 
 import com.pingsoft.mark.pojo.RespBean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = Exception.class)
     public RespBean error(Exception ex) {
         ex.printStackTrace();
-        return RespBean.error("操作发生异常！" + ex.getMessage());
+        return RespBean.error(ex.getMessage() + "操作发生异常！");
     }
 }
