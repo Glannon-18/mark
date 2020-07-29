4ec325b210e67afd5e9fceb9b8e649b3722a9183
myName <rced87@aliyun.com>
2020-07-29 14:57:14 +0800

    小组各句对类型后台代码

--- mark-web/src/main/java/com/pingsoft/mark/web/controller/GroupController.java
+++ mark-web/src/main/java/com/pingsoft/mark/web/controller/GroupController.java
@@ -1,20 +1,74 @@
 package com.pingsoft.mark.web.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.pingsoft.mark.Constant;
+import com.pingsoft.mark.pojo.Group;
+import com.pingsoft.mark.pojo.RespBean;
+import com.pingsoft.mark.pojo.RespPageBean;
+import com.pingsoft.mark.sevice.IGroupService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author wkw
  * @since 2020-07-27
  */
 @Controller
-@RequestMapping("/group")
+@RequestMapping("/system/team")
 public class GroupController {
 
+    @Resource
+    private IGroupService groupService;
+
+
+    @PostMapping("/")
+    public RespBean add(@RequestParam String name) {
+        Group group = new Group();
+        group.setName(name);
+        group.setDiscard(Constant.NOT_DELETE);
+        group.setCreateTime(LocalDateTime.now());
+        groupService.save(group);
+        return RespBean.ok("添加小组成功！");
+    }
+
+    @PutMapping("/")
+    public RespBean edit(@RequestParam String id, @RequestParam String name) {
+        UpdateWrapper<Group> groupUpdateWrapper = new UpdateWrapper<>();
+        groupUpdateWrapper.set("name", name).eq("id", Long.valueOf(id));
+        groupService.update(groupUpdateWrapper);
+        return RespBean.ok("修改小组成功！");
+    }
+
+
+    @DeleteMapping("/{id}")
+    public RespBean delete(@PathVariable String id) {
+        groupService.removeById(Long.valueOf(id));
+        return RespBean.ok("删除小组成功！");
+    }
+
+    @GetMapping("/{id}")
+    public RespBean getById(@PathVariable String id) {
+        return RespBean.ok(groupService.getById(id));
+    }
+
+
+    @GetMapping("/page")
+    public RespPageBean page(@RequestParam String currentPage, @RequestParam String name) {
+        Page<Group> groupPage = new Page<>(Integer.valueOf(currentPage), Constant.PAGE_SIZE);
+        QueryWrapper<Group> groupQueryWrapper = new QueryWrapper<>();
+        groupQueryWrapper.like("name", name);
+        Page<Group> page = groupService.page(groupPage, groupQueryWrapper);
+        return new RespPageBean(page.getTotal(), page.getRecords(), page.getSize());
+
+    }
 }
--- mark-web/src/main/java/com/pingsoft/mark/web/controller/PairTypeController.java
+++ mark-web/src/main/java/com/pingsoft/mark/web/controller/PairTypeController.java
@@ -1,20 +1,70 @@
 package com.pingsoft.mark.web.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.pingsoft.mark.Constant;
+import com.pingsoft.mark.pojo.PairType;
+import com.pingsoft.mark.pojo.RespBean;
+import com.pingsoft.mark.pojo.RespPageBean;
+import com.pingsoft.mark.sevice.IPairTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author wkw
  * @since 2020-07-27
  */
 @Controller
-@RequestMapping("/pairType")
+@RequestMapping("/system/type")
 public class PairTypeController {
 
+    @Resource
+    private IPairTypeService pairTypeService;
+
+    @PostMapping("/")
+    public RespBean add(@RequestParam String name) {
+        PairType pairType = new PairType();
+        pairType.setName(name);
+        pairType.setCreateTime(LocalDateTime.now());
+        pairType.setDiscard(Constant.NOT_DELETE);
+        pairTypeService.save(pairType);
+        return RespBean.ok("添加类型成功！");
+    }
+
+    @PutMapping("/")
+    public RespBean edit(@RequestParam String id, @RequestParam String name) {
+        UpdateWrapper<PairType> pairTypeUpdateWrapper = new UpdateWrapper<>();
+        pairTypeUpdateWrapper.set("name", name).eq("id", Long.valueOf(id));
+        pairTypeService.update(pairTypeUpdateWrapper);
+        return RespBean.ok("修改类型成功！");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean delete(@PathVariable String id) {
+        pairTypeService.removeById(Long.valueOf(id));
+        return RespBean.ok("删除小组成功！");
+    }
+
+    @GetMapping("/{id}")
+    public RespBean getById(@PathVariable String id) {
+        return RespBean.ok(pairTypeService.getById(id));
+    }
+
+    @GetMapping("/page")
+    public RespPageBean page(@RequestParam String currentPage, @RequestParam String name) {
+        Page<PairType> pairTypePage = new Page<>(Integer.valueOf(currentPage), Constant.PAGE_SIZE);
+        QueryWrapper<PairType> pairTypeQueryWrapper = new QueryWrapper<>();
+        pairTypeQueryWrapper.like("name", name);
+        Page<PairType> page = pairTypeService.page(pairTypePage, pairTypeQueryWrapper);
+        return new RespPageBean(page.getTotal(), page.getRecords(), page.getSize());
+    }
 }
